ea04e44d9941d71baf62e684e15d6b49f85ce20c
EC2 Default User <ec2-user@ip-172-31-37-237.us-east-2.compute.internal>
2018-06-21 03:33:44 +0000

    have button

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use \App\Item;
 
+use App\Http\Controllers\Controller;
+
+use App\Item;
 
 class ItemsController extends Controller
 {
@@ -42,10 +44,12 @@ class ItemsController extends Controller
     {
       $item = Item::find($id);
       $want_users = $item->want_users;
+      $have_users = $item->have_users;
 
       return view('items.show', [
           'item' => $item,
           'want_users' => $want_users,
+          'have_users' => $have_users,
       ]);
     }
 }
