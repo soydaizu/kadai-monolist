090a45a2cdb179e2fdd8c7d9f743afe0ab261968
EC2 Default User <ec2-user@ip-172-31-37-237.us-east-2.compute.internal>
2018-06-21 01:04:29 +0000

    want

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Controllers\Controller;
+
+use App\Item;
+
 class WelcomeController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $items = Item::orderBy('updated_at', 'desc')->paginate(20);
+        return view('welcome', [
+            'items' => $items,
+        ]);
     }
 }
